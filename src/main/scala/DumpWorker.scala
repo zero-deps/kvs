f06fa3f55ba5bc2da5b867f5f18807c474a80551
Oleksandr Prokopiuk <oleksandr.prokopiuk@playtech.com>
2016-02-24 15:44:24 +0200

    dump created to file with timestamp and logs filename.

--- src/main/scala/DumpWorker.scala
+++ src/main/scala/DumpWorker.scala
@@ -1,7 +1,7 @@
 package mws.rng
 
 import akka.cluster.VectorClock
-import akka.actor.{FSM, Props}
+import akka.actor.{FSM, Props, ActorLogging}
 import mws.rng.store.{BucketGet, GetBucketResp, WriteStore, BucketPut}
 import java.util.Calendar
 import scala.collection.SortedMap
@@ -10,36 +10,44 @@ import org.iq80.leveldb._
 
 case class DumpData(current: Bucket, prefList: PreferenceList, collected: List[Option[List[Data]]])
 
-class DumpWorker(buckets: SortedMap[Bucket, PreferenceList]) extends FSM[FsmState, DumpData]{
+class DumpWorker(buckets: SortedMap[Bucket, PreferenceList], local: Node) extends FSM[FsmState, DumpData] with ActorLogging{
     val leveldbFactory = org.iq80.leveldb.impl.Iq80DBFactory.factory
     val timestamp = Calendar.getInstance().getTime().toString.replaceAll(" ", "")
-    val db = leveldbFactory.open(new File(s"rng_dump_$timestamp"), new Options().createIfMissing(true))
+    val filePath = s"rng_dump_$timestamp"
+    val db = leveldbFactory.open(new File(filePath), new Options().createIfMissing(true))
     val dumpStore = context.actorOf(Props(classOf[WriteStore], db))
     val stores = SelectionMemorize(context.system)
-    val bucketsNum = context.system.settings.config.getInt("ring.buckets")
+    val lastBucket = context.system.settings.config.getInt("ring.buckets")
     startWith(ReadyCollect, DumpData(0, Nil, Nil))
     
     when(ReadyCollect){
-        case Event(Dump, data ) => 
-            data.current match {
-                case `bucketsNum` => 
-                    
-                    stop()
-                case b =>  
-                    buckets(b).foreach{n => stores.get(n, "ring_readonly_store").fold(_ ! BucketGet(b), _ ! BucketGet(b))}
-                    goto(Collecting) using(DumpData(b, buckets(b), Nil))
-            }
+        case Event(Dump, state ) => 
+            buckets(state.current).foreach{n => stores.get(n, "ring_readonly_store").fold(_ ! BucketGet(state.current), _ ! BucketGet(state.current))}
+            goto(Collecting) using(DumpData(state.current, buckets(state.current), Nil))
     }
 
-    when(Collecting){
-        case Event(GetBucketResp(b,data), state) => 
-            remove(sender().path.address, state.prefList) match {
+    when(Collecting){ 
+        case Event(GetBucketResp(b,data), state) => // TODO add timeout if any node is not responding.
+            log.info(s"receive b=$b, data = ${data}")
+            remove(if(sender().path.address.hasLocalScope) local else sender().path.address, state.prefList) match {
                 case Nil => 
                     val bucketData = mergeBucketData(
                         (data :: state.collected).flatten.foldLeft(List.empty[Data])((acc, l) => l ::: acc), Nil)
-                    dumpStore ! BucketPut(bucketData)
-                    goto(ReadyCollect) using(DumpData(state.current + 1, Nil, Nil))
-                case l => stay() using(DumpData(state.current ,l ,data :: state.collected))
+                    if(bucketData.nonEmpty) {
+                        log.info(s"bucket put $bucketData")
+                        dumpStore ! BucketPut(bucketData)}
+                    b+1 match {
+                        case `lastBucket` => 
+                            stores.get(self.path.address, "ring_hash").fold(_ ! DumpComplete(filePath), _ ! DumpComplete(filePath))
+                            log.info("Dump complete, sending path to hash")
+                            db.close
+                            stop()
+                        case nextBucket => 
+                            buckets(nextBucket).foreach{n => stores.get(n, "ring_readonly_store").fold(_ ! BucketGet(nextBucket), _ ! BucketGet(nextBucket))}
+                            stay() using(DumpData(nextBucket, buckets(nextBucket), Nil))
+                    }
+                case l => 
+                    stay() using(DumpData(state.current ,l , data :: state.collected))
             } 
     }
 
