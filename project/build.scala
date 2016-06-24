e43fc1445be35bdaf3944f456600718d8c26b477
Andrii Nemchenko <a.nemchenko@icloud.com>
2016-06-24 14:57:18 +0300

    deps

--- project/build.scala
+++ project/build.scala
@@ -7,7 +7,7 @@ object Versions {
   val scala = "2.11.8"
   val scalaz = "7.2.2"
   val pickling = "0.11.0-M2"
-  val rng = "1.0-189-gd8ffa57"
+  val rng = "1.0-191-g0ab4047"
 }
 
 object Build extends sbt.Build{
@@ -17,16 +17,20 @@ object Build extends sbt.Build{
     settings = Defaults.coreDefaultSettings ++ publishSettings ++ Seq(
       scalacOptions in Compile ++= Seq("-feature", "-deprecation", "-target:jvm-1.7"),
       javacOptions in Compile ++= Seq("-source", "1.7", "-target", "1.7"),
-      dependencyOverrides ++= Set(
-        "org.scala-lang" % "scala-compiler" % Versions.scala,
-        "org.scala-lang" % "scala-reflect" % Versions.scala,
-        "org.scala-lang" % "scala-library" % Versions.scala
-      ),
       libraryDependencies ++= Seq(
         "org.scalaz" %% "scalaz-core" % Versions.scalaz,
+
         ("org.scala-lang.modules" %% "scala-pickling" % Versions.pickling).
-          exclude("org.scala-lang.modules","scala-parser-combinators_2.11"),
+          exclude("org.scala-lang.modules","scala-parser-combinators_2.11").
+          exclude("org.scala-lang","scala-compiler").
+          exclude("org.scala-lang","scala-reflect"),
+        ("org.scala-lang" % "scala-compiler" % Versions.scala).
+          exclude("org.scala-lang.modules","scala-xml_2.11"),
+        "org.scala-lang.modules" %% "scala-xml" % "1.0.5",
+
+        "com.typesafe.akka" %% "akka-cluster" % "2.3.15" % Provided,
         "com.playtech.mws" %% "rng" % Versions.rng,
+
         "junit" % "junit" % "4.12" % Test,
         "org.scalatest" %% "scalatest" % "2.2.4" % Test,
         "com.typesafe.akka" %% "akka-testkit" % "2.3.14" % Test
