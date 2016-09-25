77c2b88129cc8a4a535bafc401db51d589fbb521
Oleksandr Prokopiuk <oleksandr.prokopiuk>
2016-09-25 20:41:54 +0300

    removed extra level between KVS and RNG

--- src/main/scala/store/distributed.scala
+++ src/main/scala/store/distributed.scala
@@ -1,11 +1,18 @@
 package mws.kvs
 package store
 
+import java.io.File
 import java.util.concurrent.TimeUnit
+import akka.event.Logging
+import akka.routing.FromConfig
+import mws.rng.store.{ReadonlyStore, WriteStore}
+import org.iq80.leveldb.{CompressionType, Options}
+import akka.pattern.ask
+
 import scala.util._
-import akka.actor.ExtendedActorSystem
+import akka.actor.{Deploy, Props, ExtendedActorSystem}
 import akka.util.ByteString
-import mws.rng.{HashRing, AckSuccess, Ack}
+import mws.rng._
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
 
@@ -14,39 +21,65 @@ object Ring {
 }
 
 class Ring(system: ExtendedActorSystem) extends Dba {
-  val rng = HashRing(system)
-
+  lazy val log = Logging(system, "hash-ring")
   val d = Duration(5, TimeUnit.SECONDS)
 
-  def put(key: String, value: V): Either[Err, V] =
-    Try(Await.result(rng.put(key,ByteString(value)),d)) match {
+  lazy val clusterConfig = system.settings.config.getConfig("akka.cluster")
+  system.eventStream
+
+  val config = system.settings.config.getConfig("ring.leveldb")
+  val nativeLeveldb:Boolean = sys.props.get("os.name") match {
+    case Some(os) if os.startsWith("Windows") =>
+      log.info("Forcing usage of Java ported LevelDB for Windows OS"); false
+    case _ => config.getBoolean("native")
+  }
+
+  val leveldbOptions = new Options().createIfMissing(true)
+  val leveldbDir = new File(config.getString("dir"))
+  var leveldb = leveldbFactory.open(leveldbDir, if (nativeLeveldb) leveldbOptions else leveldbOptions.compressionType(CompressionType.NONE))
+
+  def leveldbFactory =
+    if (nativeLeveldb) org.fusesource.leveldbjni.JniDBFactory.factory
+    else org.iq80.leveldb.impl.Iq80DBFactory.factory
+  system.actorOf(Props(classOf[WriteStore],leveldb).withDeploy(Deploy.local), name="ring_write_store")
+  system.actorOf(FromConfig.props(Props(classOf[ReadonlyStore], leveldb)).withDeploy(Deploy.local), name = "ring_readonly_store")
+
+  private val hash = system.actorOf(Props(classOf[Hash]).withDeploy(Deploy.local), name = "ring_hash")
+
+  def put(key: String, value: V): Either[Err, V] = {
+    val putF = (hash ? Put(key, ByteString(value))).mapTo[Ack]
+    Try(Await.result(putF, d)) match {
       case Success(AckSuccess) => Right(value)
       case Success(not_success: Ack) => Left(not_success.toString)
       case Failure(ex) => Left(ex.getMessage)
     }
+  }
 
-  def isReady: Future[Boolean] = rng.isReady
+  def isReady: Future[Boolean] = (hash ? Ready).mapTo[Boolean]
 
-  def get(key: String): Either[Err, V] =
-    Try(Await.result(rng.get(key),d)) match {
+  def get(key: String): Either[Err, V] = {
+    val getF = (hash ? Get(key)).mapTo[Option[Value]]
+    Try(Await.result(getF, d)) match {
       case Success(Some(v)) => Right(v.toArray)
       case Success(None) => Left(s"not_found key $key")
       case Failure(ex) => Left(ex.getMessage)
     }
+  }
 
-  def delete(key: String): Either[Err, V] =
+  def delete(key: String): Either[Err, V] = {
     get(key).fold(
       l => Left(l),
-      r => Try(Await.result(rng.delete(key),d)) match {
+      r => Try(Await.result((hash ? Delete(key)).mapTo[Ack], d)) match {
         case Success(AckSuccess) => Right(r)
         case Success(not_success) => Left(not_success.toString)
         case Failure(ex) => Left(ex.getMessage)
       }
     )
+  }
 
-  def save():Future[String] = rng.dump()
-  def load(path:String):Future[Any] = rng.load(path)
-  def iterate(path:String,foreach:(String,Array[Byte])=>Unit):Future[Any] = rng.iterate(path,foreach)
+  def save():Future[String] = (hash ? Dump).mapTo[String]
+  def load(path:String):Future[Any] = hash ? LoadDump(path)
+  def iterate(path:String,foreach:(String,Array[Byte])=>Unit):Future[Any] = hash ? IterateDump(path,foreach)
 
   def close(): Unit = ()
 }
