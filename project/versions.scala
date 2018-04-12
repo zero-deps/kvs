18d437a1f32e5d33ff7c35e581c190a99e2f2d0e
Andrii Nemchenko <a.nemchenko@icloud.com>
2018-04-12 20:36:26 +0300

    update deps

--- project/versions.scala
+++ project/versions.scala
@@ -1,9 +1,9 @@
 object Versions {
-  val scala = "2.12.4"
-  val scalaz = "7.2.19"
+  val scala = "2.12.5"
+  val scalaz = "7.2.21"
   val akka = "2.4.14.0-1-g4986967" // before updating to any version test 'sbt createPkg' in CMS project
-  val leveldb = "1.18.1"
+  val leveldb = "1.20.0"
   val logback = "1.2.3"
-  val pickling = "1.0-1-g1b17114"
+  val pickling = "1.0-2-gb05b7b9"
   val scalatest = "3.0.1"
 }
