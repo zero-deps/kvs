ee898f1fd0770e98bf040831ba6a09482850885f
Andrii Nemchenko <a.nemchenko@icloud.com>
2020-04-13 21:04:22 +0300

    demo

--- demo/src/main/scala/Run.scala
+++ demo/src/main/scala/Run.scala
@@ -1,30 +1,35 @@
 package zd.kvs
 
-import scala.util.Try
-import zd.proto.Bytes
-import akka.actor.ActorSystem
-
 object Run extends App {
+
+  import zd.gs.z._
+  import akka.actor.ActorSystem
+  val cfg = """akka.remote.netty.tcp.hostname = 127.0.0.1
+  akka.remote.netty.tcp.port = 4343
+  akka.cluster.seed-nodes = [ "akka.tcp://sys@127.0.0.1:4343" ]
+  ring.leveldb.dir = rng_data_127.0.0.1_4343"""
   import com.typesafe.config.ConfigFactory
-  val cfg = ConfigFactory.load()
-  implicit val system = ActorSystem("kvs", cfg)
-  val log = system.log
+  val system = ActorSystem("sys", ConfigFactory.parseString(cfg))
+  import zd.kvs.Kvs
   val kvs = Kvs(system)
+  import scala.util.Try
+  import scala.concurrent.Await
+  import scala.concurrent.duration._
+  Try(Await.result(kvs.onReady, Duration.Inf))
+  import zd.proto.Bytes
+  val users = Bytes.unsafeWrap(s"users${System.currentTimeMillis}".getBytes)
+  import zd.proto.api.{N, MessageCodec, encode, decode}
+  final case class User(@N(1) name: String)
+  import zd.proto.macrosapi.caseCodecAuto
+  implicit val UserC: MessageCodec[User] = caseCodecAuto[User]
+  kvs.add(users, Bytes.unsafeWrap(encode[User](User(name="John Doe"))))
+  kvs.add(users, Bytes.unsafeWrap(encode[User](User(name="Jane Doe"))))
+  import zd.gs.z._
+  kvs.all(users).flatMap(_.sequence).fold(
+      err => system.log.error(err.toString)
+    , xs => xs.foreach(x => system.log.info(decode[User](x.en.data.unsafeArray).name))
+    )
+  system.terminate()
+  Try(Await.result(system.whenTerminated, Duration.Inf))
 
-  sys.addShutdownHook {
-    println("SHUTDOWN")
-    system.terminate()
-    import scala.concurrent.Await
-    import scala.concurrent.duration.Duration
-    val _ = Try(Await.result(system.whenTerminated,Duration.Inf))
-  }
-
-  import system.dispatcher
-
-  kvs.onReady.map{ _ =>
-    val k1 = Bytes.unsafeWrap(Array[Byte](1))
-    val v1 = Bytes.unsafeWrap("hi".getBytes)
-    kvs.el.put(k1, v1)
-    println(kvs.el.get(k1).map(_.map(_.unsafeArray).map(xs => new String(xs))))
-  }
 }
